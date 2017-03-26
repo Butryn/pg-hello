819d376536287abf9f17b13838494964ac6c14ed
Roksana Butryn <Roksana Butryn>
2017-03-26 18:25:29 +0200

    Moja wizytowka

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -5,30 +5,3 @@ function init() {
 function onDeviceReady() {
 	navigator.notification.beep(1);
 }
-
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
-}
-
-function personalData() {
-
-	info =  'Hi, ' + '\n' +
-			'=====' + '\n' +
-			'Roksana Butryn    '         + '\n' + 
-			'Studnetka Informatyki Stosowanej : '   + '\n' + 
-			'Nr albumu 179849 '  + '\n';
-
-	navigator.notification.alert(info);
-	
-}
